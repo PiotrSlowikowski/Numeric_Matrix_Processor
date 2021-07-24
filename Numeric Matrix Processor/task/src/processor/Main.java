f4a872ac0e061d8cea2ba9fa4d668090b15d1fc9
PiotrSlowikowski <peter.slowikowski@hotmail.com>
2021-07-24 11:54:19 +0200

    transposition methods refactored

--- Numeric Matrix Processor/task/src/processor/Main.java
+++ Numeric Matrix Processor/task/src/processor/Main.java
@@ -31,7 +31,6 @@ public class Main {
                 displayMatrix(matrixC);
             } else if (chosenAction.equals("2")) {
                 double[][] matrixC = multiplyMatrixByConstant();
-                System.out.println("The result is:");
                 displayMatrix(matrixC);
             } else if (chosenAction.equals("3")) {
                 double[][] matrixC = multiplyMatrixes();
@@ -39,19 +38,73 @@ public class Main {
                     System.out.println("The operation cannot be performed.");
                     continue;
                 }
-                System.out.println("The result is:");
                 displayMatrix(matrixC);
             } else if (chosenAction.equals("4")) {
                 displayTransPositionMenu();
                 System.out.print("Your choice: ");
                 String transpositionType = scanner.next();
-
+                if (transpositionType.equals("1")) {
+                    double[][] mainDiagonalTransposedMatrix = mainDiagonalTransposition();
+                    displayMatrix(mainDiagonalTransposedMatrix);
+                } else if (transpositionType.equals("2")) {
+                    double[][] sideDiagonalTransposedMatrix = sideDiagonalTransposition();
+                    displayMatrix(sideDiagonalTransposedMatrix);
+                } else if (transpositionType.equals("3")) {
+                    double[][] verticalLineTransposedMatrix = verticalLineTransposition();
+                    displayMatrix(verticalLineTransposedMatrix);
+                } else if (transpositionType.equals("4")) {
+                    double[][] horizontalLineTransposedMatrix = horizontalLineTransposition();
+                    displayMatrix(horizontalLineTransposedMatrix);
+                }
             }
         }
     }
 
-    public static double[][] createMatrix(String order) {
+    public static double[][] mainDiagonalTransposition() {
+        double[][] matrix = createMatrix("");
+        double[][] mainDiagonal = new double[matrix.length][matrix[0].length];
+        for (int i = 0; i < mainDiagonal.length; i++) {
+            for (int j = 0; j < mainDiagonal[i].length; j++) {
+                mainDiagonal[i][j] = matrix[j][i];
+            }
+        }
+        return mainDiagonal;
+    }
 
+    public static double[][] sideDiagonalTransposition() {
+        double[][] matrix = createMatrix("");
+        double[][] sideDiagonal = new double[matrix.length][matrix[0].length];
+        for (int i = 0; i < sideDiagonal.length; i++) {
+            for (int j = 0; j < sideDiagonal[i].length; j++) {
+                sideDiagonal[i][j] = matrix[sideDiagonal.length - 1 - j][sideDiagonal.length - 1 - i];
+            }
+        }
+        return sideDiagonal;
+    }
+
+    public static double[][] verticalLineTransposition() {
+        double[][] matrix = createMatrix("");
+        double[][] verticalArray = new double[matrix.length][matrix[0].length];
+        for (int i = 0; i < verticalArray.length; i++) {
+            for (int j = 0; j < verticalArray[i].length; j++) {
+                verticalArray[i][j] = matrix[i][matrix[i].length - 1 - j];
+            }
+        }
+        return verticalArray;
+    }
+
+    public static double[][] horizontalLineTransposition() {
+        double[][] matrix = createMatrix("");
+        double[][] horizontalArray = new double[matrix.length][matrix[0].length];
+        for (int i = 0; i < horizontalArray.length; i++) {
+            for (int j = 0; j < horizontalArray[i].length; j++) {
+                horizontalArray[i][j] = matrix[matrix.length - 1 - i][j];
+            }
+        }
+        return horizontalArray;
+    }
+
+    public static double[][] createMatrix(String order) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter size of " + order + " matrix: ");
         int row = scanner.nextInt();
@@ -74,7 +127,6 @@ public class Main {
             main.areMatrixEqual = true;
         } else {
             main.areMatrixEqual = false;
-            // KONIEC???
         }
         double[][] matrixesSum = new double[matrix1.length][matrix1[0].length];
 
@@ -130,6 +182,7 @@ public class Main {
     }
 
     public static void displayMatrix(double[][] matrix) {
+        System.out.println("The result is:");
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
@@ -153,6 +206,4 @@ public class Main {
         System.out.println("3. Vertical line");
         System.out.println("4. Horizontal line");
     }
-
-
 }
